5eb8e684cb5a87107c108716907f5211eef46000
momokata <huijingdandan@gmail.com>
2018-01-22 17:43:05 +0800

    增加电影采集来源

--- config/sourceconfig.php
+++ config/sourceconfig.php
@@ -2,5 +2,9 @@
 return [
     '33uu'=>'www.33uudy.com',
     'zuida'=>'www.zuidazy.com',
-    'baiwan'=>'www.baiwanzy.com'
+    'baiwan'=>'www.baiwanzy.com',
+    'okzy'=>'www.okzyzy.cc',
+    '131zy'=>'www.zy131.com',
+    'qqzy'=>'caiji.000o.cc',
+    'zuiyuanzy'=>'www.ziyuanpian.com',
 ];
